e7163fe6be5289fea5b9b3f549ad84b340cf69bc
jboho <bohoj@mac.com>
2014-04-20 17:09:58 -0600

    quote data-* attribute value in docs JS per best-practices

--- docs/assets/js/application.js
+++ docs/assets/js/application.js
@@ -63,7 +63,7 @@
 
     // tooltip demo
     $('.tooltip-demo').tooltip({
-      selector: '[data-toggle=tooltip]',
+      selector: '[data-toggle="tooltip"]',
       container: 'body'
     })
 
@@ -71,7 +71,7 @@
     $('.popover-test').popover()
 
     $('.bs-docs-navbar').tooltip({
-      selector: 'a[data-toggle=tooltip]',
+      selector: 'a[data-toggle="tooltip"]',
       container: '.bs-docs-navbar .nav'
     })
 
